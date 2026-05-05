e6d429ac5cae92e39c0cd4d7d998ba55bd28e953
Carson Bennett <bennec4222@clarkstate.edu>
2026-05-05 19:53:45 -0400

    Update addnumbers.py

--- addnumbers.py
+++ addnumbers.py
@@ -2,3 +2,19 @@
 #Watch this video or follow along in class.
 #https://www.youtube.com/watch?v=S79jbCk0K1w
 
+def AddTwoNumbers(number1, number2):
+    result = number1 + number2
+    return result
+
+def PrintHelloTen():
+    for _ in range(10):
+        print("Hello")
+
+x = 5
+y = 10
+
+result = AddTwoNumbers(x, y)
+
+print(result)
+
+PrintHelloTen()
